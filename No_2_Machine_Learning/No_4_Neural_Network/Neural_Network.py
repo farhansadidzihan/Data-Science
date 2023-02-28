5c106cc5d41915ae8c39bd898403a9b46db81ce6
Farhan Sadid Zihan <101565227+FarhanZihan@users.noreply.github.com>
2023-02-28 16:17:16 +0600

    Update Neural_Network.py

--- No_2_Machine_Learning/No_4_Neural_Network/Neural_Network.py
+++ No_2_Machine_Learning/No_4_Neural_Network/Neural_Network.py
@@ -16,8 +16,28 @@ class_name = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Cost", "Sandal",
 train_images = train_images / 255
 test_images = test_images / 255
 
-print(train_images[7])
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
 
-# Plotting the Data
-plt.imshow(train_images[7], cmap=plt.cm.binary)
-plt.show()
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+prediction = model.predict(test_images[7])
+
+for image in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[image], cmap=plt.cm.binary)
+    plt.xlabel("Actual Image", class_name[test_images[image]])
+    plt.ylabel("Prediction", class_name[np.argmax(prediction[image])])
+    plt.show()
+
+# print(train_images[7])
+# pprint(train_images[7]) # Using pprint
+# # Plotting the Data
+# plt.imshow(train_images[7], cmap=plt.cm.binary)
+# plt.show()
